e9da89ac20a4b1c930ca10cb78274344f2a9153c
César González <cesargonzaleziniguez@teamitg.com>
2023-01-28 10:53:45 +0100

    show the response

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,8 +7,12 @@
           <InputText id="url" type="text" v-model="url" />
           <label for="url">Url</label>
         </span>
-        <Button label="Aceptar" />
+        <Button label="Aceptar" @click="submit()" />
       </template>
+    </Card>
+    <Card v-if="response">
+      <template #title>Resultado</template>
+      <template #content>{{ response }}</template>
     </Card>
   </div>
 </template>
@@ -18,6 +22,12 @@ import Card from "primevue/card";
 import Button from "primevue/button";
 import InputText from "primevue/inputtext";
 import { Ref, ref } from "vue";
+import axios from "axios";
 
 const url: Ref<string> = ref();
+let response: Ref<string> = ref();
+
+async function submit() {
+  response = await axios.get("http://localhost:3000/analyze/" + url.value);
+}
 </script>
