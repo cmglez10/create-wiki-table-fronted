9361403f71bc42cc03d34396c3c8d4984b9d3649
César González <cesargonzaleziniguez@teamitg.com>
2023-01-30 11:07:08 +0100

    show table

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,10 +10,86 @@
         <Button label="Aceptar" @click="submit()" />
       </template>
     </Card>
-    <Card v-if="response">
+    <Card v-if="teams">
+      <template #title>Clasificación</template>
+      <template #content>
+        <DataTable stripedRows :value="teams" responsiveLayout="scroll">
+          <Column
+            field="position"
+            header="Pos"
+            bodyClass="body-center"
+            headerClass="header-center"
+          >
+          </Column>
+          <Column>
+            <template #body="row">
+              <img :src="row.data.shield" />
+            </template>
+          </Column>
+          <Column field="name" header="Equipo" headerStyle="width:20rem">
+          </Column>
+          <Column
+            field="played"
+            header="J"
+            bodyClass="body-center"
+            headerClass="header-center"
+          >
+          </Column>
+          <Column
+            field="won"
+            header="G"
+            bodyClass="body-center"
+            headerClass="header-center"
+          ></Column>
+          <Column
+            field="drawn"
+            header="E"
+            bodyClass="body-center"
+            headerClass="header-center"
+          ></Column>
+          <Column
+            field="lost"
+            header="P"
+            bodyClass="body-center"
+            headerClass="header-center"
+          ></Column>
+          <Column
+            field="gf"
+            header="GF"
+            bodyClass="body-center"
+            headerClass="header-center"
+          ></Column>
+          <Column
+            field="ga"
+            header="GC"
+            bodyClass="body-center"
+            headerClass="header-center"
+          ></Column>
+          <Column
+            field="gd"
+            header="GD"
+            bodyClass="body-center"
+            headerClass="header-center"
+          ></Column>
+          <Column
+            field="sanction"
+            header="S"
+            bodyClass="body-center"
+            headerClass="header-center"
+          ></Column>
+          <Column
+            field="points"
+            header="Pts"
+            bodyClass="body-center"
+            headerClass="header-center"
+          ></Column>
+        </DataTable>
+      </template>
+    </Card>
+    <Card v-if="teams">
       <template #title>Resultado</template>
       <template #content>
-        <code>{{ response }}</code>
+        <code>{{ teams }}</code>
       </template>
     </Card>
   </div>
@@ -25,13 +101,29 @@ import Button from "primevue/button";
 import InputText from "primevue/inputtext";
 import { Ref, ref } from "vue";
 import axios from "axios";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+
+export interface Team {
+  position: number;
+  name: string;
+  points: number;
+  played: number;
+  won: number;
+  drawn: number;
+  lost: number;
+  gf: number;
+  ga: number;
+  gd: number;
+  sanction: number;
+}
 
 const url: Ref<string> = ref("28516");
-let response: Ref<string> = ref();
+let teams: Ref<Team[]> = ref();
 
 async function submit() {
-  response.value = (
-    await axios.get("http://localhost:3000/analyze/" + url.value)
+  teams.value = (
+    await axios.get<Team[]>("http://localhost:3000/analyze/" + url.value)
   )?.data;
 }
 </script>
